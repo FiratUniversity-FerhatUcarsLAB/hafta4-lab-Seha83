f49af959aea0a624c7010a7b48fb719251360041
Seha83 <250541021@firat.edu.tr>
2025-11-04 13:36:11 +0300

    Update OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [İbrahim Seha DOĞAN]
+ * Ogrenci No: [250541021]
+ * Tarih: [04.11.2025]
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -10,36 +10,50 @@
  */
 
 import java.util.Scanner;
-
+import java.util.Locale;
 public class OgrenciBilgi {
     public static void main(String[] args) {
         // Scanner objesi olusturun
-        
-        
+         Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US); // Nokta ile ondalık giriş için
+
         // Degisken tanimlamalari
         // String ad, soyad;
         // int ogrenciNo, yas;
         // double gpa;
-        
+              System.out.print("Adınızı giriniz: ");
+        String ad = input.nextLine();
+
+        System.out.print("Soyadınızı giriniz: ");
+        String soyad = input.nextLine();
+
+        System.out.print("Öğrenci Numaranızı giriniz: ");
+        int numara = input.nextInt();
+
+        System.out.print("Yaşınızı giriniz: ");
+        int yas = input.nextInt();
+
+        System.out.print("GPA (0.00 - 4.00) değerini giriniz (nokta kullanın): ");
+        double gpa = input.nextDouble();
         
         // Kullanicidan bilgileri alin
         System.out.println("=== OGRENCI BILGI SISTEMI ===");
         System.out.println();
         
         // Ad
-        
+         System.out.printf("║ Ad: %-30s║%n", ad);
         
         // Soyad
-        
+        System.out.printf("║ Soyad: %-27s║%n", soyad);
         
         // Ogrenci No
-        
+        System.out.printf("║ Numara: %-26d║%n", numara);
         
         // Yas
         
-        
+        System.out.printf("║ Yaş: %-30d║%n", yas);
         // GPA
-        
+        System.out.printf("║ GPA: %-29.2f║%n", gpa);
         
         // Bilgileri ekrana yazdirin
         System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
@@ -55,6 +69,6 @@ public class OgrenciBilgi {
         
         
         // Scanner'i kapatin (önemli pratik)
-        
+        input.close();
     }
 }
